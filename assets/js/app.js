6b3cfda367b6ee6ab57e583ab78e5d93ea9a6f71
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-06-21 19:06:23 +0300

    add essentials to project

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBartoXcvlmH1wu6lLuFXcpGEqiSjIoXng",
+  authDomain: "giftbox-40985.firebaseapp.com",
+  databaseURL: "https://giftbox-40985.firebaseio.com",
+  storageBucket: "giftbox-40985.appspot.com",
+};
+firebase.initializeApp(config);
